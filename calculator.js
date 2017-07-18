d9ea5eec19936166005dd366b813b5de10709b3d
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-07-18 13:26:36 -0500

    added basic functions

---
+++ calculator.js
@@ -0,0 +1,56 @@
+let product = "";
+let sum = "";
+let difference = "";
+let quotient = "";
+
+/*
+  Create a function that multiplies two numbers
+  passed in as arguments. Return the product.
+ */
+function multiplyInput(first, second) {
+  product = first * second;
+  return product; 
+}
+
+
+/*
+  Create a function that adds two numbers
+  passed in as arguments. Return the sum.
+ */
+function addInput(first, second) {
+  sum = first + second;
+  return product; 
+}
+
+/*
+  Create a function that subtracts two numbers
+  passed in as arguments. Return the difference.
+ */
+function subtractInput(first, second) {
+  difference = first - second;
+  return product; 
+}
+
+/*
+  Create a function that divides two numbers
+  passed in as arguments. Return the quotient.
+ */
+function divideInput(first, second) {
+  quotient = first / second;
+  return product; 
+}
+
+
+  // Create a function that accepts three arguments.
+  //   1. First number
+  //   2. Second number
+  //   3. A function that performs an operation on them
+
+function doTheCalc (first, second, )
+
+
+
+
+
+  // Return the value of the operation.
+ 
