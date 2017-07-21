100c0ea6c1f3650c7c00e69c98a746ad227065e6
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-07-20 23:24:00 -0500

    I have completed the exercise.

--- calculator.js
+++ calculator.js
@@ -1,44 +1,51 @@
-let product = "";
-let sum = "";
-let difference = "";
-let quotient = "";
+console.log ("works");
 
-/*
-  Create a function that multiplies two numbers
-  passed in as arguments. Return the product.
- */
-function multiplyInput(first, second) {
-  product = first * second;
-  return product; 
-}
+let firstInput = document.getElementById("first-value");
+let secondInput = document.getElementById("second-value");
+let output = document.getElementById("output");
+
+let subtractButton = document.getElementById("subtract-button");
+let multiplyButton = document.getElementById("multiply-button");
+let divideButton = document.getElementById("divide-button");
+let additionButton = document.getElementById("addition-button");
 
 
-/*
-  Create a function that adds two numbers
-  passed in as arguments. Return the sum.
- */
-function addInput(first, second) {
-  sum = first + second;
-  return product; 
-}
+  /*
+    Create a function that multiplies two numbers
+    passed in as arguments. Return the product.
+   */
+  function multiplyInput(first, second) {
+    product = first * second;
+    output.innerHTML = `<h2>${product}</h2>`;
+  }
 
-/*
-  Create a function that subtracts two numbers
-  passed in as arguments. Return the difference.
- */
-function subtractInput(first, second) {
-  difference = first - second;
-  return product; 
-}
 
-/*
-  Create a function that divides two numbers
-  passed in as arguments. Return the quotient.
- */
-function divideInput(first, second) {
-  quotient = first / second;
-  return product; 
-}
+  /*
+    Create a function that adds two numbers
+    passed in as arguments. Return the sum.
+   */
+  function addInput(first, second) {
+    sum = parseInt(first) + parseInt(second);
+    output.innerHTML = `<h2>${sum}</h2>`;
+  }
+
+  
+    // Create a function that subtracts two numbers
+    // passed in as arguments. Return the difference.
+   
+  function subtractInput(first, second) {
+    difference = first - second;
+    output.innerHTML = `<h2>${difference}</h2>`;
+  }
+
+  /*
+    Create a function that divides two numbers
+    passed in as arguments. Return the quotient.
+   */
+  function divideInput(first, second) {
+    quotient = first / second;
+    output.innerHTML = `<h2>${quotient}</h2>`;
+  }
 
 
   // Create a function that accepts three arguments.
@@ -46,11 +53,25 @@ function divideInput(first, second) {
   //   2. Second number
   //   3. A function that performs an operation on them
 
-function doTheCalc (first, second, )
+function assignOperator(first, second, operation) {
+  operation(first, second);
+};
 
 
-
+subtractButton.addEventListener("click", function() {
+  assignOperator(firstInput.value, secondInput.value, subtractInput);
+});
+multiplyButton.addEventListener("click", function() {
+  assignOperator(firstInput.value, secondInput.value, multiplyInput);
+});
+divideButton.addEventListener("click", function() {
+  assignOperator(firstInput.value, secondInput.value, divideInput);
+});
+additionButton.addEventListener("click", function() {
+  assignOperator(firstInput.value, secondInput.value, addInput);
+});
 
 
   // Return the value of the operation.
+  // This is built into each operators' function... printing straight to the page in an H2.
  
